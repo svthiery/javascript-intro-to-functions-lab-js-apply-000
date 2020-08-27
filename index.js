13fbb5221717469273e86231bddd1c54b991569c
Sarah Kain <sarahvkain@gmail.com>
2020-08-27 15:57:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function logShout(string) {
   console.log(upperString);
 }
 
+function logWhisper(string) {
+  let lowerString = string.toLowerCase();
+  console.log(lowerString);
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase === string){
+    return "I can't hear you!";
+  } else if(string.toUpperCase === string){
+    return "YES INDEED!";
+  } else if(string === "I love you, Grandma."){
+    return "I love you, too.";
+  }
+}
